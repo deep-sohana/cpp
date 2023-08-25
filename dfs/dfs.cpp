bef7f50e60e0f60b5ccf0f4fc547e6acc1ea022b
Deep Sohana <141994657+deep-sohana@users.noreply.github.com>
2023-08-25 18:11:27 +1000

    Add files via upload

--- dfs/dfs.cpp
+++ dfs/dfs.cpp
@@ -1,1 +1,40 @@
+#include "dfs.h"
+#include <string>
+using namespace std;
 
+
+void GraphTraversal::printPath(std::vector<const Node *> &path)
+{
+    if (path.empty()) {
+        return; 
+    }
+
+    string pathStr = "START: ";
+    for (size_t i = 0; i < path.size(); ++i) {
+        pathStr += to_string(path[i]->getNodeID()) + "->";
+    }
+    pathStr += "END";
+
+    paths.insert(pathStr);
+};
+
+
+void GraphTraversal::DFS(set<const Node *> &visited, vector<const Node *> &path, const Node *src, const Node *dst)
+{
+    visited.insert(src);
+    path.push_back(src);
+
+    if (src == dst) {
+        printPath(path);
+    } else {
+        for (const Edge *edge : src->getOutEdges()) {
+            const Node *neighbor = edge->getDst();
+            if (visited.find(neighbor) == visited.end()) {
+                DFS(visited, path, neighbor, dst);
+            }
+        }
+    }
+
+    path.pop_back();
+    visited.erase(src);
+}
